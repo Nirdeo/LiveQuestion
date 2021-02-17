1ffd45939d7b1e3806c372e09a47f77073287dab
TristanNiel <tristan.niel73@gmail.com>
2021-02-17 16:48:36 +0100

    PHP Written

--- connexion.php
+++ connexion.php
@@ -14,17 +14,47 @@
 </head>
 <body>
 
+	<?php
+	require("PHP/connexion.php");
+
+	session_start();
+
+	$co = connexionBdd();
+
+	if (isset($_POST["submit"])) {
+		$username = $_POST["username"];
+		$password = hash("sha256", $_POST["password"]);
+		$query = $co->prepare('SELECT * FROM utilisateurs WHERE pseudo=:login and mot_de_passe=:pass');
+		$query->bindParam(":login", $username);
+		$query->bindParam(":pass", $password);
+		$query->execute();
+		$result = $query->fetchall();
+		$rows = $query->rowCount();
+		if ($rows == 1) {
+			$_SESSION["username"] = $username;
+			echo "<p>Vous vous etes connecter au nom de" . $_SESSION['username'] . "</p>";
+		}else {
+			$message = "Le nom d'utilisateur ou le mot de passe est incorrect.";
+		}
+	}
+
+	?>
+
 		<div class="loginBox">
 			<h1>Se connecter</h1>
-			<form>
+			<form action="" method="post" name="login">
 				<p>Nom d'Utilisateur</p>
-				<input type="text" name="" placeholder="Enter votre nom d'utilisateur">
+				<input type="text" name="username" placeholder="Enter votre nom d'utilisateur">
 				<p>mot de passe</p>
-				<input type="password" name="" placeholder="Enter votre mot de passe">
-				<input type="submit" name="" value="valider">
+				<input type="password" name="password" placeholder="Enter votre mot de passe">
+				<input type="submit" name="submit" value="valider">
 				<p>Vous n'avez pas de compte ? <a href="#"> S'inscrire maintenant !</a></p>
 			</form>
 		</div>
-
+	<?php
+	if (! empty($message)) {
+		echo "<p>$message</p>";
+	}
+	?>
 </body>
 </html>
