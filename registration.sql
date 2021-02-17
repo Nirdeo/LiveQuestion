408505409ccc119364b7becde6c081e2c5702258
Victor DeDomenico <victordedomenico.du60@gmail.com>
2021-02-17 16:56:01 +0100

    Update registration.sql
    Mise à jour de l'auto-incrémentation

--- registration.sql
+++ registration.sql
@@ -1,5 +1,5 @@
 CREATE TABLE utilisateurs (
-id INT PRIMARY KEY,
+id INT PRIMARY KEY AUTO_INCREMENT,
 pseudo VARCHAR(255),
 email VARCHAR(255),
 mot_de_passe VARCHAR(255),
@@ -9,11 +9,11 @@ date_inscription DATETIME,
 role VARCHAR(255)
 );
 CREATE TABLE categories (
-id INT PRIMARY KEY,
+id INT PRIMARY KEY AUTO_INCREMENT,
 nom VARCHAR(255)
 );
 CREATE TABLE questions (
-id INT PRIMARY KEY,
+id INT PRIMARY KEY AUTO_INCREMENT,
 titre VARCHAR(45),
 categorie_id INT,
 auteur_id INT,
@@ -22,8 +22,8 @@ FOREIGN KEY (categorie_id) REFERENCES categories(id),
 FOREIGN KEY (auteur_id) REFERENCES utilisateurs(id)
 );
 CREATE TABLE repondre (
-utilisateurs_id INT,
-questions_id INT,
+utilisateurs_id INT AUTO_INCREMENT,
+questions_id INT AUTO_INCREMENT,
 date DATETIME,
 reponse VARCHAR(255),
 PRIMARY KEY (utilisateurs_id, questions_id),
