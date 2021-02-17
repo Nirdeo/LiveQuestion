f70c767a29b3cf378bce789ef4d833a9eb94b4fa
TristanNiel <tristan.niel73@gmail.com>
2021-02-17 11:56:54 +0100

    Files Changements

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.1/css/all.css">
-	<script type="text/javascript" src="indexScript.js"></script>
+	<script type="text/javascript" src="script/indexScript.js"></script>
 
 </head>
 <body>
