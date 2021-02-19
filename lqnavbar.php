02243d0c82eea9ac28d37a7d13ac1da1bbfd8400
Victor DeDomenico <victordedomenico.du60@gmail.com>
2021-02-19 14:34:31 +0100

    Espace vue membre
    Ajout vue membre

---
+++ lqnavbar.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <title>LiveQuestion</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="style/lqnavbar.css">
+    <script src="script.js"></script>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.1/css/all.css">
+</head>
+
+<body>
+    <!-- NAVBAR -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="../index.php">Livequestion</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor03" aria-controls="navbarColor03" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarColor03">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="hConnexion.php">Les questions</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#" data-toggle="modal" data-target="#questionModal">Poser une question</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="../EspaceMembre/espacemembre.php">Mon profil</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</body>
+
+</html>
