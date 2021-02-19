8b6926a2fe913bd0f1e63240043e75a4eb8d25f6
Victor DeDomenico <victordedomenico.du60@gmail.com>
2021-02-19 14:06:03 +0100

    Changement sur l'espace membre

--- EspaceMembre/espacemembre.php
+++ EspaceMembre/espacemembre.php
@@ -38,10 +38,7 @@
         <div class="container">
             <h3>Espace Membre</h3>
             <h1>Bienvenue dans votre espace membre NomUtilisateur !</h1><br>
-<<<<<<< Updated upstream
-=======
             <!-- CARD -->
->>>>>>> Stashed changes
             <div class="card mb-3">
                 <h3 class="card-header">Modifier mes informations <i class="fas fa-cog"></i></h3>
                 <svg for="file" xmlns="http://www.w3.org/2000/svg" class="d-block user-select-none" width="100%" height="200" aria-label="Placeholder: Mon avatar" focusable="false" role="img" preserveAspectRatio="xMidYMid slice" viewBox="0 0 318 180" style="font-size:1.125rem;text-anchor:middle">
