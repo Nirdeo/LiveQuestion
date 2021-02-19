76220249ffc9904748b89ea906bbb2026dc37795
TristanNiel <tristan.niel73@gmail.com>
2021-02-19 09:14:41 +0100

    fix-navbar-padding

--- navbar.php
+++ navbar.php
@@ -2,40 +2,43 @@
 	<link rel="stylesheet" type="text/css" href="style/navbarStyle.css">
 </head>
 
-<nav class="navbar navbar-expand-md navbar-dark">
-	<a class="navbar-brand font-weight-bold" href="index.php">Saint Vincent BTS1</a>
-	<div class="collapse navbar-collapse justify-content-end" id="collapsibleNavbar">
-		<ul class="navbar-nav">
-			<li class="nav-item dropdown">
-				<a href="" class="nav-link dropdown-toggle" id="navbardrop" data-toggle="dropdown">Lien 1</a>
-				<div class="dropdown-menu">
-					<a href="" class="dropdown-item">Lien 1</a>
-					<a href="" class="dropdown-item">Lien 2</a>
-				</div>
-			</li>
-			<li class="nav-item dropdown">
-				<a href="" class="nav-link dropdown-toggle" id="navbardrop" data-toggle="dropdown">Lien 2</a>
-				<div class="dropdown-menu">
-					<a href="" class="dropdown-item">Lien 1</a>
-					<a href="" class="dropdown-item">Lien 2</a>
-				</div>
-			</li>
-			<li class="nav-item dropdown">
-				<a href="" class="nav-link dropdown-toggle" id="navbardrop" data-toggle="dropdown">Lien 3</a>
-				<div class="dropdown-menu">
-					<a href="" class="dropdown-item">Lien 1</a>
-					<a href="" class="dropdown-item">Lien 2</a>
-				</div>
-			</li>
-			<li class="nav-item dropdown">
-				<a href="" class="nav-link dropdown-toggle" id="navbardrop" data-toggle="dropdown">Lien 4</a>
-				<div class="dropdown-menu">
-					<a href="" class="dropdown-item">Lien 1</a>
-					<a href="" class="dropdown-item">Lien 2</a>
-				</div>
-			</li>
-		</ul>
-	</div>
-	<a href="connexion.php" class="navbar-link btn bouton">Se Connecter</a>
-	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar"><span class="navbar-toggler-icon"></span></button>
-</nav>
+
+<div class="paddingNavbar">
+	<nav class="navbar navbar-expand-md navbar-dark">
+		<a class="navbar-brand font-weight-bold" href="index.php">Saint Vincent BTS1</a>
+		<div class="collapse navbar-collapse justify-content-end" id="collapsibleNavbar">
+			<ul class="navbar-nav">
+				<li class="nav-item dropdown">
+					<a href="" class="nav-link dropdown-toggle" id="navbardrop" data-toggle="dropdown">Lien 1</a>
+					<div class="dropdown-menu">
+						<a href="" class="dropdown-item">Lien 1</a>
+						<a href="" class="dropdown-item">Lien 2</a>
+					</div>
+				</li>
+				<li class="nav-item dropdown">
+					<a href="" class="nav-link dropdown-toggle" id="navbardrop" data-toggle="dropdown">Lien 2</a>
+					<div class="dropdown-menu">
+						<a href="" class="dropdown-item">Lien 1</a>
+						<a href="" class="dropdown-item">Lien 2</a>
+					</div>
+				</li>
+				<li class="nav-item dropdown">
+					<a href="" class="nav-link dropdown-toggle" id="navbardrop" data-toggle="dropdown">Lien 3</a>
+					<div class="dropdown-menu">
+						<a href="" class="dropdown-item">Lien 1</a>
+						<a href="" class="dropdown-item">Lien 2</a>
+					</div>
+				</li>
+				<li class="nav-item dropdown">
+					<a href="" class="nav-link dropdown-toggle" id="navbardrop" data-toggle="dropdown">Lien 4</a>
+					<div class="dropdown-menu">
+						<a href="" class="dropdown-item">Lien 1</a>
+						<a href="" class="dropdown-item">Lien 2</a>
+					</div>
+				</li>
+			</ul>
+		</div>
+		<a href="connexion.php" class="navbar-link btn bouton">Se Connecter</a>
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar"><span class="navbar-toggler-icon"></span></button>
+	</nav>
+</div>
