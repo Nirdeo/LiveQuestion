ec51957c688216b3de587f0e8b87397ef87c788f
Nirdeo <victordedomenico.du60@gmail.com>
2021-04-27 14:13:39 +0200

    Ajustement
    Mise à jour de l'indentation et suppression des liens inutiles

--- inscription.php
+++ inscription.php
@@ -1,72 +1,66 @@
 <!DOCTYPE html>
 <html>
+   <head>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>LiveQuestion</title>
+      <link rel="stylesheet" type="text/css" href="styles/inscriptionStyle.css">
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+      <script type="text/javascript" src="script.js"></script>
+      <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.1/css/all.css">
+   </head>
+   <body>
+      <?php
+         require("PHP/connexion.php");
 
-<head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>LiveQuestion</title>
-	<link rel="stylesheet" type="text/css" href="styles/inscriptionStyle.css">
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-	<script type="text/javascript" src="script.js"></script>
-	<link href='https://fonts.googleapis.com/css?family=Lato' rel='stylesheet' type='text/css'>
-	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.1/css/all.css">
-</head>
-
-<body>
-
-	<?php
-
-	require("PHP/connexion.php");
-
-	if (isset($_POST["submit"])) {
-		if (isset($_POST["username"], $_POST["email"], $_POST["genre"], $_POST["password"], $_POST["passwordC"])) {
-			if ($_POST["password"] == $_POST["passwordC"]) {
-				$username = $_POST["username"];
-				$email = $_POST["email"];
-				$genre = $_POST["genre"];
-				$password = hash("sha256", $_POST["password"]);
-				$date = date('Y-m-d H:i:s');
-				$co = connexionBdd();
-				$query = $co->prepare("INSERT into utilisateurs (pseudo, email, mot_de_passe, genre, date_inscription, role) VALUES (:username, :email, :pass, :genre, :dateAjd, 'membre')");
-				$query->bindParam(":username", $username);
-				$query->bindParam(":email", $email);
-				$query->bindParam(":genre", $genre);
-				$query->bindParam(":pass", $password);
-				$query->bindParam(":dateAjd", $date);
-				$query->execute();
-				if ($query) {
-					echo "Inscription valider";
-				}
-			}else {
-				echo "Les mots de passes ne correspondent pas";
-			}
-		}
-	}else {
-		?>
-	<div class="wrapper">
-		<div class="loginBox">
-			<h1>S'inscrire</h1>
-			<form action="" method="post" name="register">
-				<input type="text" name="username" placeholder="Enter votre nom d'utilisateur">
-				<input type="email" name="email" placeholder="Enter votre Adresse mail">
-				<p>Genre :</p>
-				<div class="radioInput">
-					<label for="homme">Homme</label>
-					<input type="radio" name="genre" value="H">
-					<label for="femme">Femme</label>
-					<input type="radio" name="genre" value="F">
-				</div>
-				<input type="password" name="password" placeholder="Enter votre mot de passe">
-				<input type="password" name="passwordC" placeholder="Confirmer votre mot de passe">
-				<input type="submit" name="submit" value="valider">
-				<p>Vous avez deja un compte ? <a href="connexion.php"> Se connecter !</a></p>
-			</form>
-		</div>
-	</div>
-	<?php
-	}
-	?>
-
+         if (isset($_POST["submit"])) {
+         	if (isset($_POST["username"], $_POST["email"], $_POST["genre"], $_POST["password"], $_POST["passwordC"])) {
+         		if ($_POST["password"] == $_POST["passwordC"]) {
+         			$username = $_POST["username"];
+         			$email = $_POST["email"];
+         			$genre = $_POST["genre"];
+         			$password = hash("sha256", $_POST["password"]);
+         			$date = date('Y-m-d H:i:s');
+         			$co = connexionBdd();
+         			$query = $co->prepare("INSERT into utilisateurs (pseudo, email, mot_de_passe, genre, date_inscription, role) VALUES (:username, :email, :pass, :genre, :dateAjd, 'membre')");
+         			$query->bindParam(":username", $username);
+         			$query->bindParam(":email", $email);
+         			$query->bindParam(":genre", $genre);
+         			$query->bindParam(":pass", $password);
+         			$query->bindParam(":dateAjd", $date);
+         			$query->execute();
+         			if ($query) {
+         				echo "Inscription validée";
+         			}
+         		}else {
+         			echo "Les mots de passes ne correspondent pas";
+         		}
+         	}
+         }else {
+         	?>
+      <div class="wrapper">
+         <div class="loginBox">
+            <h1>S'inscrire</h1>
+            <form action="" method="post" name="register">
+               <input type="text" name="username" placeholder="Enter votre nom d'utilisateur">
+               <input type="email" name="email" placeholder="Enter votre Adresse mail">
+               <p>Genre :</p>
+               <div class="radioInput">
+                  <label for="homme">Homme</label>
+                  <input type="radio" name="genre" value="H">
+                  <label for="femme">Femme</label>
+                  <input type="radio" name="genre" value="F">
+               </div>
+               <input type="password" name="password" placeholder="Enter votre mot de passe">
+               <input type="password" name="passwordC" placeholder="Confirmer votre mot de passe">
+               <input type="submit" name="submit" value="valider">
+               <p>Vous avez deja un compte ? <a href="connexion.php"> Se connecter !</a></p>
+            </form>
+         </div>
+      </div>
+      <?php
+         }
+         ?>
 </html>
--- navbar.php
+++ navbar.php
@@ -1,56 +1,50 @@
 <head>
-	<link rel="stylesheet" type="text/css" href="styles/navbarStyle.css">
+   <link rel="stylesheet" type="text/css" href="styles/navbarStyle.css">
 </head>
-
-
-<<nav class="navbar navbar-expand-lg navbar-dark ">
-			<a class="navbar-brand" href="index.php">Saint Vincent BTS1</a>
-			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor"
-				aria-controls="navbarColor" aria-expanded="false" aria-label="Toggle navigation">
-				<span class="navbar-toggler-icon"></span>
-			</button>
-			
-			<div class="collapse navbar-collapse" id="navbarColor">
-				
-				<ul class="navbar-nav mr-auto">
-				
-					<li class="nav-item dropdown">
-						<a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true"
-							aria-expanded="false">Lien 1</a>
-						<div class="dropdown-menu">
-							<a class="dropdown-item" href="#">Lien 1</a>
-							<a class="dropdown-item" href="#">Lien 2</a>
-						</div>
-					</li>
-					<li class="nav-item dropdown">
-						<a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true"
-							aria-expanded="false">Lien 2</a>
-						<div class="dropdown-menu">
-							<a class="dropdown-item" href="#">Lien 1</a>
-							<a class="dropdown-item" href="#">Lien 2</a>
-						</div>
-					</li>
-					<li class="nav-item dropdown">
-						<a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true"
-							aria-expanded="false">Lien 3</a>
-						<div class="dropdown-menu">
-							<a class="dropdown-item" href="#">Lien 1</a>
-							<a class="dropdown-item" href="#">Lien 2</a>
-						</div>
-					</li>
-					<li class="nav-item dropdown">
-						<a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button"
-							aria-haspopup="true" aria-expanded="false">Lien 4</a>
-						<div class="dropdown-menu">
-							<a class="dropdown-item" href="#">Lien 1</a>
-							<a class="dropdown-item" href="#">Lien 2</a>
-						</div>
-					</li>
-				</ul>
-
-				
-				
-			</div>
-			
-		</nav>
-		<a class="btn btn-secondary" type="submit" href="redirection.php">Se Connecter</a>
+<
+<nav class="navbar navbar-expand-lg fixed-top navbar-dark bg-dark">
+   <a class="navbar-brand" href="index.php">Saint Vincent BTS1</a>
+   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor"
+      aria-controls="navbarColor" aria-expanded="false" aria-label="Toggle navigation">
+   <span class="navbar-toggler-icon"></span>
+   </button>
+   <div class="collapse navbar-collapse" id="navbarColor">
+      <ul class="navbar-nav mr-auto">
+         <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true"
+               aria-expanded="false">Lien 1</a>
+            <div class="dropdown-menu">
+               <a class="dropdown-item" href="#">Lien 1</a>
+               <a class="dropdown-item" href="#">Lien 2</a>
+            </div>
+         </li>
+         <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true"
+               aria-expanded="false">Lien 2</a>
+            <div class="dropdown-menu">
+               <a class="dropdown-item" href="#">Lien 1</a>
+               <a class="dropdown-item" href="#">Lien 2</a>
+            </div>
+         </li>
+         <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true"
+               aria-expanded="false">Lien 3</a>
+            <div class="dropdown-menu">
+               <a class="dropdown-item" href="#">Lien 1</a>
+               <a class="dropdown-item" href="#">Lien 2</a>
+            </div>
+         </li>
+         <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true"
+               aria-expanded="false">Lien 4</a>
+            <div class="dropdown-menu">
+               <a class="dropdown-item" href="#">Lien 1</a>
+               <a class="dropdown-item" href="#">Lien 2</a>
+            </div>
+         </li>
+         <li class="nav-item dropdown">
+            <a class="btn btn-secondary" type="submit" href="redirection.php">Se Connecter</a>
+         </li>
+      </ul>
+   </div>
+</nav>
